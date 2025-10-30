11aef893e6e28d0c5023645c1e19f0bbbf79fa82
JoskSzoFil <joskaszfi@gmail.com>
2025-10-29 22:12:08 -0700

    feat: 5 hechos de recetas en formato json

---
+++ #5-SLD/hechos.py
@@ -0,0 +1,79 @@
+recetas = {
+    # Vegana
+    "spaghetti_con_pure_de_berenjena": {
+        "ingredientes": {
+            "proteina": {"tofu": "1/2 taza troceado"},
+            "carbohidrato": {"spaghetti_integral": "250 g"},
+            "verdura": {"berenjena": "1 unidad", "ajo": "2 dientes"},
+            "liquido": {"aceite_de_oliva": "2 cucharadas", "jugo_de_limon": "1 cucharada"},
+            "condimento": {"sal": "al gusto"},
+            "hierba": {"perejil": "al gusto"}
+        }
+    },
+
+    # Vegana
+    "tacos_de_champiñones": {
+        "ingredientes": {
+            "proteina": {},
+            "carbohidrato": {"tortillas_de_maiz": "9 unidades"},
+            "aceite": {"aceite_vegetal": "2 cucharadas"},
+            "verdura": {
+                "champiñones": "500g",
+                "cebolla": "1 pieza",
+                "chiles_serranos": "2 unidades",
+                "limones": "3 unidades"
+            },
+            "condimento": {"sal": "al gusto", "pimienta": "al gusto"},
+            "hierba": {"cilantro": "1/2 taza"},
+            "otros": {"ajo": "2 dientes"}
+        }
+    },
+
+    # Vegetariana
+    "quesadillas_flor_calabaza": {
+        "ingredientes": {
+            "proteina": {},
+            "carbohidrato": {"tortilla_de_maiz": "8 unidades"},
+            "verdura": {"flor_calabaza": "1 taza"},
+            "lacteo": {"queso_fresco": "100 g"},
+            "aceite": {"aceite_vegetal": "1 cucharada"},
+            "condimento": {"sal": "al gusto"},
+            "otros": {"ajo": "1 diente"}
+        }
+    },
+
+    "tacos_al_pastor": {
+        "ingredientes": {
+            "proteina": {"cerdo": "500 g"},
+            "carbohidrato": {"tortilla_de_maiz": "8 unidades"},
+            "verdura": {"cebolla": "1/2 unidad", "cilantro": "al gusto", "piña": "1/2 taza picada"},
+            "condimento": {"sal": "al gusto"},
+            "otros": {"achiote": "2 cucharadas", "chiles_secos": "3 unidades", "ajo": "2 dientes"},
+            "liquido": {"jugo_de_piña": "1/4 taza"},
+            "aceite": {"aceite_vegetal": "2 cucharadas"}
+        }
+    },
+
+    "pozole": {
+        "ingredientes": {
+            "proteina": {"carne_de_cerdo": "1kg (maciza y costilla)", "Huevo": "1 unidad"},
+            "carbohidrato": {"maiz_pozolero": "500g", "tostadas": "12 unidades"},
+            "verdura": {
+                "cebolla": "1 pieza",
+                "cebolla_picada": "1/2 taza",
+                "lechuga": "1 taza (finamente picada)",
+                "rábanos": "1/2 taza (en rodajas)",
+                "limones": "4 unidades",
+                "chiles_guajillo": "6 unidades",
+                "ajos": "4 dientes"
+            },
+            "liquido": {"agua": "3 litros"},
+            "condimento": {"sal": "al gusto", "orégano_seco": "1 cucharada"}
+        }
+    }
+}
+
+proteinas = {
+    "Animal": ["cerdo", "carne_de_cerdo"],
+    "Vegetal": ["tofu"]
+}
